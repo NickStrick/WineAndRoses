715425d6f8794ea8aff6a623a1b01c4c02a6e649
Nickolas Stricker <43568886+NickStrick@users.noreply.github.com>
2020-03-17 18:05:13 -0500

    added images to header

--- roses/src/App.js
+++ roses/src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import './App.scss';
+import greenBoquet from './images/green-bouquet.jpg';
+import roseVase from './images/rose-vase.jpg';
 
 function App() {
   return (
@@ -9,6 +11,10 @@ function App() {
       </header>
       <div className="container">
         <h1 className='title'>Wine and Roses</h1>
+        <div className='cta-images'>
+          <img src={roseVase} />
+          <img src={greenBoquet} />
+        </div>
         <div className="info">
           <h2 className='contact-title'>BOOK A PRIVATE CLASS</h2>
           <a className='msg' href='https://m.me/flowersandclasses?fbclid=IwAR3IgNUGCt8RWVpBAjfbKPDpaM_xgi4r1uMzCJa0_EtgQ5gnPNLlJtieh-E'>Send a Message</a>

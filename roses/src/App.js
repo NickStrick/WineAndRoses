dbf065198965ef703413c7b77906ee4f3c6a07d1
Nickolas Stricker <43568886+NickStrick@users.noreply.github.com>
2020-03-17 17:52:45 -0500

    moved html

--- roses/src/App.js
+++ roses/src/App.js
@@ -11,8 +11,9 @@ function App() {
         <h1 className='title'>Wine and Roses</h1>
         <div className="info">
           <h2 className='contact-title'>BOOK A PRIVATE CLASS</h2>
+          <a className='msg' href='https://m.me/flowersandclasses?fbclid=IwAR3IgNUGCt8RWVpBAjfbKPDpaM_xgi4r1uMzCJa0_EtgQ5gnPNLlJtieh-E'>Send a Message</a>
           <div className='contacts'>
-            <a href='https://m.me/flowersandclasses?fbclid=IwAR3IgNUGCt8RWVpBAjfbKPDpaM_xgi4r1uMzCJa0_EtgQ5gnPNLlJtieh-E'>Send a Message</a>
+
             <div className='item'>
               <label>Phone</label>
               <p>(773) 209-4805</p>
